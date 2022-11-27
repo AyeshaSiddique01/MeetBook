7168317d9c8e51d1c08ff9d7a32fb37e507c8ca1
AyeshaSiddique01 <89693571+AyeshaSiddique01@users.noreply.github.com>
2022-11-27 19:34:19 +0500

    Connection error removed

--- MeetBook/Models/MeetBookContext.cs
+++ MeetBook/Models/MeetBookContext.cs
@@ -28,7 +28,7 @@ namespace MeetBook.Models
             {
                 //warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 // optionsBuilder.UseSqlServer(@"Server=localhost,1440; Database=master; User=sa; Password=Strong@Passw0rd");
-                optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MeetBook;Integrated Secugtirity=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MeetBook;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             }
         }
         public override int SaveChanges()
